261a26622163d71f8539741737c70c1f166ad4d9
Eugene Molari <eugelogic@gmail.com>
2017-08-12 22:48:04 +0100

    Add shortcode

--- youtube-useful-video-gallery.php
+++ youtube-useful-video-gallery.php
@@ -32,6 +32,9 @@ require_once(plugin_dir_path(__FILE__) . '/inc/youtube-useful-video-gallery-scri
 // Load Custom Post Type
 require_once(plugin_dir_path(__FILE__) . '/inc/youtube-useful-video-gallery-cpt.php');
 
+// Load Shortcodes
+require_once(plugin_dir_path(__FILE__) . '/inc/youtube-useful-video-gallery-shortcodes.php');
+
 // Check if admin and include admin Scripts
 if (is_admin()){
   // Load Post Fields
